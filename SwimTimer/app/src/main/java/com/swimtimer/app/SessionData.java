b71990f80fa68049c6e841c454b9dffb04a03451
EnricoRoberto <enricoroberto@gmail.com>
2026-03-18 11:54:31 +0100

    Update SessionData.java

--- SwimTimer/app/src/main/java/com/swimtimer/app/SessionData.java
+++ SwimTimer/app/src/main/java/com/swimtimer/app/SessionData.java
@@ -5,11 +5,13 @@ public class SessionData {
     private String name, id, photoPath;
     private long date, totalTime;
     private List<Long> laps;
+    private boolean imported;
 
     public SessionData(String name, long date, long totalTime, List<Long> laps) {
         this.name = name; this.date = date;
         this.totalTime = totalTime; this.laps = laps;
         this.id = String.valueOf(date);
+        this.imported = false;
     }
 
     public String getId() { return id; }
@@ -18,6 +20,8 @@ public class SessionData {
     public long getTotalTime() { return totalTime; }
     public List<Long> getLaps() { return laps; }
     public String getPhotoPath() { return photoPath; }
+    public boolean isImported() { return imported; }
     public void setName(String n) { name = n; }
     public void setPhotoPath(String p) { photoPath = p; }
+    public void setImported(boolean i) { imported = i; }
 }
